58b62992c3ba377bb7679dc23a6cbc1da3d6c923
SelimYuksel <yukselselim26@gmail.com>
2024-05-10 12:01:05 +0300

    Commit changes

--- FrmDoctorPanel.Designer.cs
+++ FrmDoctorPanel.Designer.cs
@@ -214,9 +214,9 @@
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.ForeColor = System.Drawing.Color.Beige;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "FrmDoctorPanel";
-            this.Text = "FrmDoctorPanel";
+            this.Text = "DOKTOR PANEL";
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
