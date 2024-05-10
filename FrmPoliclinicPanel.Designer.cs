fe2a2bfdd8ac3738be47e1a0d5daeb59b0a3d554
SelimYuksel <yukselselim26@gmail.com>
2024-05-10 12:15:31 +0300

    Commit changes

--- FrmPoliclinicPanel.Designer.cs
+++ FrmPoliclinicPanel.Designer.cs
@@ -146,9 +146,9 @@
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.ForeColor = System.Drawing.Color.Beige;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "FrmPoliclinicPanel";
-            this.Text = "FrmPoliclinicPanel";
+            this.Text = "POLİKLİNİK PANELİ ";
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
